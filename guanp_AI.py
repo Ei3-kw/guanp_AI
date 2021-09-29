0041fc66932a4d2b4c42d3dcb41b5e8fe15619d3
Ei3-kw <e013.kw@gmail.com>
2021-09-29 16:32:00 +1000

    singles

--- guanp_AI.py
+++ guanp_AI.py
@@ -18,9 +18,14 @@ TWO = 15
 REGULAR = [FOUR]+[FIVE]+[SIX]+[SEVEN]+[EIGHT]+[NINE]+[TEN]+[J]+[Q]+[K]
 deck = random.shuffle(4*REGULAR + 3*([A]+[THREE]) + [TWO] + [THE_THREE])
 my_hand = deck[::3]
+player_1 = deck[1::3]
+player_2 = deck[2::3]
 
 class Rules:
-	"""docstring for Rule"""
+	"""
+	find out most combos according to rules
+	excluding things related to singles
+	"""
 	def __init__(self, my_hand, arg):
 		self.my_hand = my_hand
 		self.my_hand_adjusted = my_hand
@@ -58,6 +63,7 @@ class Rules:
 
 	def basic_ladders(self, target_list) -> List[List[int, ...], ...]:
 		"""
+		general format of ascending ladders 
 		"""
 		ladders = []
 		for c in target_list:
@@ -72,6 +78,8 @@ class Rules:
 
 	def get_ladders(self) -> List[List[int, ...], ...]:
 		"""
+		return all ladders (len >= 5) 
+		update what's left in my_hand excluding ladders
 		"""
 		ladders = []
 		for ladder in self.basic_ladders(self.my_hand_adjusted):
@@ -82,6 +90,10 @@ class Rules:
 		return ladders
 
 	def find_doubles(self) -> List[int, ...]:
+		"""
+		return all doubles 
+		update what's left in my_hand excluding doubles
+		"""
 		doubles = []
 		# check for doubles
 		for i in (REGULAR + [THREE] + A):
@@ -91,16 +103,24 @@ class Rules:
 				self.my_hand_adjusted.remove(i)
 		return doubles
 
-	def get_double_ladders(self) -> List[List[int, ...], ...]:
+	def get_double_ladders(self) -> List[List[List[int,int], ...], ...]:
 		"""
+		return list of triple (ladders) if any, empty list otherwise
 		"""
 		double_ladders = []
 		doubles = self.find_doubles()
 		for ladder in self.basic_ladders(doubles):
-			double_ladders.append(2*ladder)
+			for i in ladder:
+				double_ladder = []
+				double_ladder.append(3 * [i])
+			double_ladders.append(double_ladder)
 		return double_ladders
 
 	def find_triples(self) -> List[int, ...]:
+		"""
+		return all triples 
+		update what's left in my_hand excluding triples
+		"""
 		triples = []
 		# check for triples\ A
 		for i in (REGULAR + [THREE]):
@@ -109,14 +129,61 @@ class Rules:
 				self.update_my_hand(i)
 		return triples
 
-	def get_triple_ladders(self) -> List[List[int, ...], ...]:
+	def get_triple_ladders(self) -> List[List[List[int,int,int] ...], ...]:
 		"""
+		return list of triple (ladders) if any, empty list otherwise
 		"""
 		triple_ladders = []
 		triples = self.find_triples()
 		for ladder in self.basic_ladders(triples):
-			triple_ladders.append(3*ladder)
+			for i in ladder:
+				triple_ladder = []
+				triple_ladder.append(3 * [i])
+			triple_ladders.append(triple_ladder)
 		return triple_ladders
+
+	def triple_double(self) -> List[List[int, ...], ...]:
+		"""
+		"""
+		triple_double_ladders = []
+		triple_ladders = self.get_triple_ladders()
+		double_ladders = self.get_double_ladders()
+		for i in triple_ladders: 
+			for j in double_ladders:
+				if i.len() = j.len():
+					triple_double_ladders.append(i+j)
+		return triple_double_ladders
+
+class Analysis:
+
+	def __init__(self, rules: Rules, won: bool, played: bool):
+		self.rules_1 = rules
+		self.rules_2 = rules
+		self.rules_3 = rules
+		self.rules_4 = rules
+		self.rules_5 = rules
+		self.rules_6 = rules
+		self.start_first = won or (not played and rules.has_spade_three())
+
+	def append_singles(self, singles, bombs, triples, triple_doubles):
+		j = 0
+		for i in triple_doubles:
+			j += i.len()/2
+		# calculate the amount can't append
+		x = bombs.len() + triples.len() - j
+		y = 0
+		num_singles = max(0, singles.len()-x)
+		while y < num_singles:
+			singles.pop(y)
+			y += 1
+		return singles
+
+	def single_1(self) -> List[List[int], int]:
+		bombs = 
+		singles = self.rules_1.my_hand_adjusted.sort()
+		return [singles.pop]
+
+	def abs_single(self) -> List[Optional(int)]:
 
 
 		
